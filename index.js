72f40d41bd529fab8158e5bdabac1e6a58afe644
laryea <kalexgun@gmail.com>
2025-04-19 11:58:02 +0000

    Added a score feature and switched to map function

--- index.js
+++ index.js
@@ -14,68 +14,89 @@ const nextButton = document.querySelector(".next-btn");
 
 const displayElement = document.querySelector(".display");
 
+const scoreUpdate = document.getElementById("score-update")
 
 let currentQuestionIndex = 0;
 
 let questions = [];
+
+let score = {
+  wins: 0,
+  losses: 0
+}
 
 const fetchQuiz = async() => {
   try {
     const response = await fetch("https://opentdb.com/api.php?amount=30&category=9&difficulty=easy&type=boolean");
     const quiz = await response.json();
     
-    questions = quiz.results;
-    
+    questions = quiz.results.map((question)=>{
+      return {
+        question: question.question,
+        correct_answer: question.correct_answer,
+        incorrect_answers: question.incorrect_answers
+      }
+    })
+  
     displayQuestion();
     
   } catch (error) {
     console.error("Error fetching quiz:", error);
     questionElement.textContent = "Error loading quiz. Please try again.";
   }
-};
 
+  function displayQuestion(){
 
-const displayQuestion = () => {
-
-  if (questions.length > 0 && currentQuestionIndex < questions.length) {
-
-    const currentQuestion = questions[currentQuestionIndex];
-    
-    questionElement.textContent = currentQuestion.question;
-    
-    nextButton.style.display = "none";
-    
-    displayElement.textContent = "";
-  } else {
-    questionElement.textContent = "Quiz complete! Refresh to start over.";
-    trueButton.style.display = "none";
-    falseButton.style.display = "none";
-    nextButton.style.display = "none";
-  }
-};
-
-const checkAnswer = (selectedAnswer) => {
-  const correctAnswer = questions[currentQuestionIndex].correct_answer;
+    if (questions.length > 0 && currentQuestionIndex < questions.length) {
   
-  if (selectedAnswer === correctAnswer) {
-    displayElement.textContent = "Correct✅ You are awesome! 👏🏽";
-  } else {
-    displayElement.textContent = "Wrong❌ You will get the next one. Keep going 💪🏽";
+      const currentQuestion = questions[currentQuestionIndex];
+      
+      questionElement.textContent = currentQuestion.question;
+      
+      nextButton.style.display = "none";
+      
+      displayElement.textContent = "";
+    } else {
+      questionElement.textContent = "Quiz complete! Refresh to start over.";
+      trueButton.style.display = "none";
+      falseButton.style.display = "none";
+      nextButton.style.display = "none";
+    }
+  };
+  
+  const checkAnswer = selectedAnswer => {
+    const correctAnswer = questions[currentQuestionIndex].correct_answer;
+  
+    if (selectedAnswer === correctAnswer) {
+      displayElement.textContent = "Correct✅ You are awesome! 👏🏽"; 
+    } else {
+      displayElement.textContent = "Wrong❌ You will get the next one. Keep going 💪🏽";
+    }
+    nextButton.style.display = "flex";
+    
+    if(selectedAnswer === correctAnswer){
+      score.wins += 1;
+      scoreUpdate.innerHTML = `<p>Wins: ${score.wins}</p>
+      <p>Losses: ${score.losses}</p>
+      `
+    }else if(selectedAnswer !== correctAnswer){
+      score.losses +=1
+      scoreUpdate.innerHTML = `<p>Wins: ${score.wins}</p>
+      <p>Losses: ${score.losses}</p>`
+    };
   }
   
-  nextButton.style.display = "flex";
+  trueButton.addEventListener('click', () => {
+    checkAnswer(trueButton.value);
+  });
+  
+  falseButton.addEventListener('click', () => {
+    checkAnswer(falseButton.value);
+  });
+  
+  nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    displayQuestion();
+  });
 };
-
-trueButton.addEventListener('click', () => {
-  checkAnswer(trueButton.value);
-});
-
-falseButton.addEventListener('click', () => {
-  checkAnswer(falseButton.value);
-});
-
-nextButton.addEventListener("click", () => {
-  currentQuestionIndex++;
-  displayQuestion();
-});
 fetchQuiz();
